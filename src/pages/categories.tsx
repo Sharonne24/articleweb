f6c4642f19b21bcea8c437139b5dfaad28d98b28
Panesar Kenya <it@panesar.co.ke>
2024-02-29 14:29:35 +0300

    feat: categories page finalization

---
+++ src/pages/categories.tsx
@@ -0,0 +1,10 @@
+import ContentWrapper from '@/components/layout/wrapper';
+import CategoriesBox from '@/features/categories/categories-box';
+
+export default function CategoriesPage() {
+  return (
+    <ContentWrapper>
+      <CategoriesBox />
+    </ContentWrapper>
+  );
+}
