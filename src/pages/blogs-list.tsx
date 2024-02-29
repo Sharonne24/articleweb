9bf036b9bb108d480992dfadf9d13bbbbede6560
Panesar Kenya <it@panesar.co.ke>
2024-02-29 15:21:40 +0300

    feat: page to create and render list of blogs

---
+++ src/pages/blogs-list.tsx
@@ -0,0 +1,10 @@
+import ContentWrapper from '@/components/layout/wrapper';
+import BlogsListBox from '@/features/blogs/blogs-list-box';
+
+export default function BlogsListPage() {
+  return (
+    <ContentWrapper>
+      <BlogsListBox />
+    </ContentWrapper>
+  );
+}
