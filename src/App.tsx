d5a5c62be75914f99cd7926b0ca3eef64124a2e7
Panesar Kenya <it@panesar.co.ke>
2024-02-29 15:11:56 +0300

    feat: define routes for protected area

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from '@/components/ui/sonner';
 
 import Home from './pages/home';
 import BlogDetails from './pages/blog-details';
 import Login from './pages/login';
+import Dashboard from './pages/dashboard';
+import AppLayout from './components/layout/app-layout';
+import Authors from './pages/authors';
+import CreateAuthor from './pages/create-author';
+import ProtectedRoute from './components/layout/protected-route';
+import CategoriesPage from './pages/categories';
+import BlogsListPage from './pages/blogs-list';
+import BlogForm from './pages/blog-form';
 
 function App() {
   return (
@@ -11,7 +20,23 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<BlogDetails />} />
         <Route path="/login" element={<Login />} />
+
+        <Route
+          element={
+            <ProtectedRoute>
+              <AppLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/authors" element={<Authors />} />
+          <Route path="/authors/new" element={<CreateAuthor />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+          <Route path="/blogs/list" element={<BlogsListPage />} />
+          <Route path="/blogs/new" element={<BlogForm />} />
+        </Route>
       </Routes>
+      <Toaster position="top-center" />
     </BrowserRouter>
   );
 }
