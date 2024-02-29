9a69cee65dd81bde20bbba0cf02b8a7a257cf0ad
Panesar Kenya <it@panesar.co.ke>
2024-02-29 15:32:23 +0300

    feat: page to create blog

---
+++ src/pages/blog-form.tsx
@@ -0,0 +1,10 @@
+import ContentWrapper from '@/components/layout/wrapper';
+import CreateBlog from '@/features/blogs/blog-form';
+
+export default function BlogForm() {
+  return (
+    <ContentWrapper>
+      <CreateBlog />
+    </ContentWrapper>
+  );
+}
