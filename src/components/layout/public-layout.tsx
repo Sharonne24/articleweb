3d8286e884f30eb3cfda5dc06bb414eff4ddbf91
Panesar Kenya <it@panesar.co.ke>
2024-02-29 23:54:58 +0300

    feat: layout for blogs page

---
+++ src/components/layout/public-layout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+
+import PublicHeader from './public-header';
+import BlogsSidebar from './blogs-sidebar';
+import Footer from './footer';
+
+export default function PublicLayout() {
+  return (
+    <div className="h-dvh flex flex-col">
+      <PublicHeader />
+      <div className="flex-1 flex">
+        <BlogsSidebar />
+        <div className="px-6 md:px-8 lg:px-12 py-4 md:py-6">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
