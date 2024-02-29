e5e89098c1f50e26aa38427a244d419c5ebe2be9
Panesar Kenya <it@panesar.co.ke>
2024-02-29 15:06:20 +0300

    feat: render author name

--- src/features/dashboard/dashboard-box.tsx
+++ src/features/dashboard/dashboard-box.tsx
@@ -1,9 +1,19 @@
+import { titleCase } from '@/lib/utils';
+import { useUser } from '../auth/use-user';
 import DashboardCards from './dashboard-cards';
 
 export default function Dashboardbox() {
+  const { user } = useUser();
+
+  if (!user) return null;
   return (
     <div className="space-y-4">
-      <h1 className="text-base md:text-2xl font-bold">Welcome back, User</h1>
+      <h1 className="text-base md:text-2xl font-bold">
+        Welcome back,{' '}
+        <span className="text-primary font-light">
+          {titleCase(user.user_metadata.fullName)}
+        </span>
+      </h1>
       <DashboardCards />
     </div>
   );
