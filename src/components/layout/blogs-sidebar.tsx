c6d6755fe4658d0f52c4161558a416a717f967ad
Panesar Kenya <it@panesar.co.ke>
2024-03-01 01:28:54 +0300

    fix: set sidebar not to shrink

--- src/components/layout/blogs-sidebar.tsx
+++ src/components/layout/blogs-sidebar.tsx
@@ -3,7 +3,7 @@ import Categories from './categories';
 
 export default function BlogsSidebar() {
   return (
-    <aside className="hidden md:block md:w-1/4 py-4 md:py-6 px-4 space-y-4">
+    <aside className="hidden md:block md:w-1/4 shrink-0 py-4 md:py-6 px-4 space-y-4">
       <SearchInput placeholder="Search articles..." className="w-full" />
       <Categories />
     </aside>
