f807c7493a9388c2f40bfbcc0d5b8eec2f52d4a8
nicmackenzie <nicmackenzie@live.com>
2024-02-29 22:17:01 +0300

    feat: image uploader component

--- src/components/ui/image-uploader.tsx
+++ src/components/ui/image-uploader.tsx
@@ -1,4 +1,85 @@
-export default function ImageUploader() {
+import { useState, ChangeEvent } from 'react';
+import { createId } from '@paralleldrive/cuid2';
+import { X } from 'lucide-react';
+
+import { toast } from 'sonner';
+
+import { supabase, supabaseUrl } from '@/lib/supabase';
+import clsx from 'clsx';
+
+interface ImageUploaderProps {
+  onImageChange: (url: string) => void;
+  disabled?: boolean;
+}
+
+export default function ImageUploader({
+  onImageChange,
+  disabled,
+}: ImageUploaderProps) {
+  const [imageDetails, setImageDetails] = useState<{
+    uploaded: boolean;
+    imageUrl: null | string;
+  }>({
+    uploaded: false,
+    imageUrl: null,
+  });
+
+  function handleImageUpload(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files?.length) return null;
+
+    const imageName = `${createId()}-${e.target.files[0].name}`.replaceAll(
+      '/',
+      ''
+    );
+    const imagePath = `${supabaseUrl}/storage/v1/object/public/blog-images/${imageName}`;
+    supabase.storage
+      .from('blog-images')
+      .upload(imageName, e.target.files[0])
+      .then(response => {
+        const { error } = response;
+        if (error) {
+          console.log(error);
+          toast.error(
+            `😞 Unable to upload selected image. Ensure image doesn't exceed 4MB`,
+            {
+              duration: 5000,
+            }
+          );
+        } else {
+          setImageDetails({ imageUrl: imagePath, uploaded: true });
+          onImageChange(imagePath);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        toast.error(`😞 Unable to upload selected image.`, { duration: 5000 });
+      });
+  }
+
+  function handleImageRemove() {
+    setImageDetails({ imageUrl: null, uploaded: false });
+    onImageChange('');
+  }
+
+  if (imageDetails.imageUrl) {
+    return (
+      <div className="relative w-fit h-fit">
+        <img
+          src={imageDetails.imageUrl}
+          alt="blog image"
+          className="rounded-md object-cover max-h-48 w-auto"
+        />
+        <button
+          onClick={handleImageRemove}
+          disabled={disabled}
+          className="border-none outline-none absolute -right-2 -top-2 flex items-center justify-center z-10 size-4 rounded-full bg-destructive"
+        >
+          <X className="size-3 text-destructive-foreground" />
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
       <div className="text-center">
@@ -17,7 +98,10 @@ export default function ImageUploader() {
         <div className="mt-4 flex text-sm leading-6 text-gray-600">
           <label
             htmlFor="file-upload"
-            className="relative cursor-pointer rounded-md bg-white font-semibold text-sky-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-sky-600 focus-within:ring-offset-2 hover:text-indigo-500"
+            className={clsx(
+              'relative cursor-pointer rounded-md bg-white font-semibold text-sky-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-sky-600 focus-within:ring-offset-2 hover:text-indigo-500',
+              { 'pointer-events-none': disabled }
+            )}
           >
             <span>Upload a file</span>
             <input
@@ -25,7 +109,7 @@ export default function ImageUploader() {
               name="file-upload"
               type="file"
               className="sr-only"
-              onChange={e => console.log(e.target.value)}
+              onChange={handleImageUpload}
             />
           </label>
           {/* <p className="pl-1">or drag and drop</p> */}
