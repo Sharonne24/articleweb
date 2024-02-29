14c3a36e3c0b6b2203939ee4342943e5500b2ae0
Panesar Kenya <it@panesar.co.ke>
2024-03-01 01:29:07 +0300

    feat: footer for website

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,12 @@
+import Container from './container';
+import Logo from './logo';
+
+export default function Footer() {
+  return (
+    <footer className="py-4 bg-secondary">
+      <Container>
+        <Logo />
+      </Container>
+    </footer>
+  );
+}
