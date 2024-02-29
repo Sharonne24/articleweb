e233065f717f022bb86125386face9b10a42107f
Panesar Kenya <it@panesar.co.ke>
2024-02-29 14:23:35 +0300

    chore: add types

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from './database.types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 // eslint-disable-next-line
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
