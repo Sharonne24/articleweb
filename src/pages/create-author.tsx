2b1821ec85391d6325a5bdbdabb7ed50d53da156
Panesar Kenya <it@panesar.co.ke>
2024-02-29 12:46:49 +0300

    feat: create authors

---
+++ src/pages/create-author.tsx
@@ -0,0 +1,10 @@
+import ContentWrapper from '@/components/layout/wrapper';
+import AuthorForm from '@/features/authors/author-form';
+
+export default function CreateAuthor() {
+  return (
+    <ContentWrapper>
+      <AuthorForm />
+    </ContentWrapper>
+  );
+}
