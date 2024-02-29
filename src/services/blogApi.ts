5e11df529eba3d33d0580ebc70038984f2813eda
nicmackenzie <nicmackenzie@live.com>
2024-02-29 22:16:36 +0300

    feat: api to create blog

---
+++ src/services/blogApi.ts
@@ -0,0 +1,23 @@
+import { FormType } from '@/features/blogs/blog-form';
+import { supabase } from '@/lib/supabase';
+
+export async function createBlog(details: FormType) {
+  const { category, content, imageUrl, title } = details;
+  const { data, error } = await supabase
+    .from('blogs')
+    .insert([
+      {
+        title: title.trim().toLowerCase(),
+        image_url: imageUrl,
+        category_id: Number(category),
+        content,
+      },
+    ])
+    .select();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
