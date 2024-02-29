8cd2252c37365695eea2b8828406fac2c6ff27b2
Panesar Kenya <it@panesar.co.ke>
2024-02-29 12:43:23 +0300

    feat: get user details

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,33 @@
 import { supabase } from '@/lib/supabase';
+import { UserRole } from '@/index';
+
+interface UserDetails {
+  name: string;
+  password: string;
+  email: string;
+  role: UserRole;
+  imageUrl?: string;
+}
+
+export async function createUser(details: UserDetails) {
+  const { data, error } = await supabase.auth.signUp({
+    email: details.email,
+    password: details.password,
+    options: {
+      data: {
+        role: details.role,
+        imageUrl: null,
+        fullName: details.name.toLowerCase(),
+      },
+    },
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
 
 export async function signIn({
   email,
@@ -17,4 +46,21 @@ export async function signIn({
   }
 
   return data;
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+  return data?.user;
+}
+
+export async function logOut() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) throw new Error(error.message);
 }
