3ae8c89d823648063bff04b60dcc1f847a821393
Panesar Kenya <it@panesar.co.ke>
2024-03-01 21:40:29 +0300

    feat: footer component for website

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,12 +1,50 @@
+import { Link } from 'react-router-dom';
+
 import Container from './container';
 import Logo from './logo';
+import Newsletter from './newsletter';
+import { Icons } from '../ui/icons';
 
 export default function Footer() {
   return (
-    <footer className="py-4 bg-secondary">
-      <Container>
-        <Logo />
-      </Container>
+    <footer>
+      <div className="py-4 bg-secondary">
+        <Container>
+          <Logo />
+          <Newsletter />
+        </Container>
+      </div>
+      <div>
+        <Container>
+          <div className="py-4 flex flex-col-reverse gap-2 md:flex-row items-center md:justify-between">
+            <p className="text-xs text-muted-foreground">
+              &copy; {new Date().getFullYear()} All rights reserved
+            </p>
+            <ul className="flex items-center gap-4">
+              <li>
+                <Link to="https:twitter.com" target="_blank">
+                  <Icons.Twitter className="size-6" />
+                </Link>
+              </li>
+              <li>
+                <Link to="https://www.linkedin.com/" target="_blank">
+                  <Icons.LinkedIn className="size-6" />
+                </Link>
+              </li>
+              <li>
+                <Link to="https://www.facebook.com/" target="_blank">
+                  <Icons.Facebook className="size-6" />
+                </Link>
+              </li>
+              <li>
+                <Link to="https://www.instagram.com/" target="_blank">
+                  <Icons.Instagram className="size-6" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </div>
     </footer>
   );
 }
