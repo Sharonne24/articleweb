16f7e240f9712c333fa8f18ca55a5b83fb40aa28
Panesar Kenya <it@panesar.co.ke>
2024-03-02 16:12:30 +0300

    feat: hide blog categories on single blog

--- src/components/layout/blogs-sidebar.tsx
+++ src/components/layout/blogs-sidebar.tsx
@@ -1,11 +1,18 @@
+import { useParams } from 'react-router-dom';
 import SearchInput from '../ui/search-input';
 import Categories from './categories';
 
 export default function BlogsSidebar() {
+  const params = useParams();
+  const hasParams = Object.keys(params).length > 0;
   return (
     <aside className="hidden md:block md:w-1/4 shrink-0 py-4 md:py-6 px-4 space-y-4">
-      <SearchInput placeholder="Search articles..." className="w-full" />
-      <Categories />
+      {!hasParams && (
+        <>
+          <SearchInput placeholder="Search articles..." className="w-full" />
+          <Categories />
+        </>
+      )}
     </aside>
   );
 }
