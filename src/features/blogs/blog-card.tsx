f002832079307faaddf43eac15345dae01497fca
Panesar Kenya <it@panesar.co.ke>
2024-03-02 09:11:08 +0300

    refactor: wrap around link component

--- src/features/blogs/blog-card.tsx
+++ src/features/blogs/blog-card.tsx
@@ -2,8 +2,10 @@ import { Badge } from '@/components/ui/badge';
 import { Card, CardContent } from '@/components/ui/card';
 import { CustomAvatar } from '@/components/ui/custom-avatar';
 import { formatDateLong } from '@/lib/utils';
+import { Link } from 'react-router-dom';
 
 interface BlogCardProps {
+  id: string;
   blogUrl: string;
   title: string;
   category: string;
@@ -14,6 +16,7 @@ interface BlogCardProps {
 }
 
 export default function BlogCard({
+  id,
   blogUrl,
   title,
   category,
@@ -23,35 +26,37 @@ export default function BlogCard({
   authorImageUrl,
 }: BlogCardProps) {
   return (
-    <Card className="rounded-sm overflow-hidden">
-      <img
-        src={blogUrl}
-        alt={`cover for ${title}`}
-        className="object-cover aspect-video"
-      />
-      <CardContent className="px-4 py-2 space-y-3">
-        <Badge variant="tag" className="capitalize">
-          {category}
-        </Badge>
-        <h3 className="text-sm md:text-base font-semibold font-display capitalize">
-          {title}
-        </h3>
-        <p className="text-sm text-muted-foreground line-clamp-3">
-          {description}
-        </p>
-        <div className="flex items-center gap-4">
-          <CustomAvatar
-            imageUrl={authorImageUrl ?? undefined}
-            userName={authorName}
-          />
-          <div className="space-y-0.5">
-            <div className="text-sm font-medium capitalize">{authorName}</div>
-            <p className="text-muted-foreground text-xs">
-              {formatDateLong(createdAt)}
-            </p>
+    <Link to={`/blogs/${id}`}>
+      <Card className="rounded-sm overflow-hidden">
+        <img
+          src={blogUrl}
+          alt={`cover for ${title}`}
+          className="object-cover aspect-video"
+        />
+        <CardContent className="px-4 py-2 space-y-3">
+          <Badge variant="tag" className="capitalize">
+            {category}
+          </Badge>
+          <h3 className="text-sm md:text-base font-semibold font-display capitalize">
+            {title}
+          </h3>
+          <p className="text-sm text-muted-foreground line-clamp-3">
+            {description}
+          </p>
+          <div className="flex items-center gap-4">
+            <CustomAvatar
+              imageUrl={authorImageUrl ?? undefined}
+              userName={authorName}
+            />
+            <div className="space-y-0.5">
+              <div className="text-sm font-medium capitalize">{authorName}</div>
+              <p className="text-muted-foreground text-xs">
+                {formatDateLong(createdAt)}
+              </p>
+            </div>
           </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
