52e9f04eac103e47e2e139e2235a4c1be96dc19a
Panesar Kenya <it@panesar.co.ke>
2024-03-02 15:26:05 +0300

    feat: fetch selected blog

--- src/pages/blog-details.tsx
+++ src/pages/blog-details.tsx
@@ -1,3 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
+import BlogDetail from '@/features/blogs/blog-detail';
+import BlogDetailSkeleton from '@/features/blogs/blog-detail-skeleton';
+import { getBlog } from '@/services/blogApi';
+import BlogNotFound from '@/features/blogs/blog-not-found';
+
 export default function BlogDetails() {
-  return <div>BlogDetails</div>;
+  const { id } = useParams();
+
+  const { isLoading, data: blog } = useQuery({
+    queryKey: ['blogs', id],
+    queryFn: () => getBlog(id),
+  });
+
+  if (isLoading) return <BlogDetailSkeleton />;
+
+  if (!blog) return <BlogNotFound />;
+
+  const formattedBlog = {
+    id,
+    title: blog.title,
+    description: blog.description,
+    publishedDate: blog.published_date,
+    blogPoster: blog.image_url,
+    categoryId: blog.category_id,
+    category: blog.categories?.category as string,
+    content: blog.content,
+    author: {
+      id: blog.users?.id as string,
+      imageUrl: blog.users?.image_url,
+      fullName: blog.users?.full_name as string,
+    },
+  };
+
+  return <BlogDetail blog={formattedBlog} />;
 }
