f78809c23b70689fc8e60c455ae4219bc97ae8b5
Sharonne24 <sawuor395@gmail.com>
2024-03-14 23:07:05 +0300

    change1

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabaseUrl = 'https://yuiemhobzygguslqmheq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9 eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1aWVtaG9ienlnZ3VzbHFtaGVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDM4NjYsImV4cCI6MjAyNjAxOTg2Nn0.m7lLI84wQBuLwnxZ752FzGPHgukeX8Xk0a6tW9xTLAc';
 
 // eslint-disable-next-line
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
