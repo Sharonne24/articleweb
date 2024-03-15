e3198a77c28be79a6a5cdaa6605423e618b3aefe
Panesar Kenya <it@panesar.co.ke>
2024-03-15 10:23:17 +0300

    feat: add login link

--- src/components/layout/public-header.tsx
+++ src/components/layout/public-header.tsx
@@ -6,13 +6,18 @@ export default function PublicHeader() {
   return (
     <header className="bg-secondary ">
       <Container>
-        <Link to="/">
-          <img
-            src="/logo/kcj-horizontal.png"
-            alt="Logo"
-            className="h-24 w-auto"
-          />
-        </Link>
+        <div className="flex items-center justify-between">
+          <Link to="/">
+            <img
+              src="/logo/kcj-horizontal.png"
+              alt="Logo"
+              className="h-24 w-auto"
+            />
+          </Link>
+          <Link to="/login" className="text-primary font-medium">
+            Login
+          </Link>
+        </div>
       </Container>
     </header>
   );
