29122b29ec6d022b028e3eb413408ee1e8e49c35
nicmackenzie <nicmackenzie@live.com>
2024-03-15 19:16:08 +0300

    refactor: remove user name

--- src/features/dashboard/dashboard-box.tsx
+++ src/features/dashboard/dashboard-box.tsx
@@ -1,4 +1,3 @@
-import { titleCase } from '@/lib/utils';
 import { useUser } from '../auth/use-user';
 import DashboardCards from './dashboard-cards';
 
@@ -8,12 +7,7 @@ export default function Dashboardbox() {
   if (!user) return null;
   return (
     <div className="space-y-4">
-      <h1 className="text-base md:text-2xl font-bold">
-        Welcome back,{' '}
-        <span className="text-primary font-light">
-          {titleCase(user.user_metadata.fullName)}
-        </span>
-      </h1>
+      <h1 className="text-base md:text-2xl font-bold">Welcome back</h1>
       <DashboardCards />
     </div>
   );
